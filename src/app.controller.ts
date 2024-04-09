f736f1a26e160e2dfdadc71ea0ddc2ff2d4f7942
johamja <johamfranco318@gmail.com>
2024-04-08 19:59:32 -0500

    Listening routes for each emergency

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from "@nestjs/common";
 import { AppService } from './app.service';
+import { request } from "express";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,29 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post("/Category1Emergency")
+  Category1Emergency(@Req() req: Request): void {
+    console.log(req.body)
+    this.appService.Category1Emergency();
+  }
+
+  @Post("/Category1EmergencyFamily")
+  Category1EmergencyFamily(@Req() req: Request): void {
+    console.log(req.body)
+    this.appService.Category1EmergencyFamily();
+  }
+
+  @Post("/Category2Emergency")
+  Category2Emergency(@Req() req: Request): void {
+    console.log(req.body)
+    this.appService.Category2Emergency();
+  }
+
+  @Post("/Category3Emergency")
+  Category3Emergency(@Req() req: Request): void {
+    console.log(req.body)
+    this.appService.Category3Emergency();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,22 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Server OK';
+  }
+
+  Category1Emergency(): void {
+
+  }
+
+  Category1EmergencyFamily(): void {
+
+  }
+
+  Category2Emergency(): void {
+
+  }
+
+  Category3Emergency(): void {
+
   }
 }
